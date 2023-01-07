09a5d7cadec5c38a88037b7c7d5e8e5f2719b4b5
SEONMl <sm000117@naver.com>
2023-01-07 15:55:29 +0900

    js, html, css 사용하여 구구단 예제 만들기

---
+++ healing-js/gugudan.js
@@ -0,0 +1,9 @@
+
+for(var i=1;i<10;i++){
+    document.write("<div class=\"gugu\">");
+    document.write("<h3>"+i+"단</h3>");
+    for(var j=1;j<10;j++){
+        document.write(i+" X "+j+" = "+i*j+"<br>");
+    }
+    document.write("</div>")
+}
