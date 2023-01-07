ae53a1f1b1ca0c3810602075d75162287cd6fb33
SEONMl <sm000117@naver.com>
2023-01-07 15:55:08 +0900

    자바스크립트 기본 문법

---
+++ healing-js/basic.js
@@ -0,0 +1,17 @@
+var number // 모든 변수, 자료형 선언 시 사용
+/*
+자료형 
+숫자형, 문자열, 논리형 / 배열, 객체 / undefined, null
+*/
+
+document.write("3 == \"3\"       ", 3=="3", "<br>");
+document.write("3 === \"3\"      ", 3==="3", "<br>");
+document.write("3 != \"3\"       ", 3!="3", "<br>"); 
+document.write("3 !== \"3\"      ", 3!=="3", "<br>");
+/*
+연결 연산자
+== : 피연산자가 서로 같으면 true
+=== : 피연산자도 같고 자료형도 같으면 true
+
+그외 if, switch, for, while, do-while 동일
+*/
