c16f1ef419556fe31750f9874dc9838b04c38f1e
SEONMl <sm000117@naver.com>
2023-01-07 22:09:46 +0900

    자바스크립트 Object 정리

---
+++ healing-js/object.js
@@ -0,0 +1,26 @@
+var numArr = new Array();
+var numArr = new Array(5);
+var charArr = ['a','b','c'];
+var numArr = Array(1,2,3,4,5);
+
+numArr.concat(charArr); // 배열끼리 합치는 메서드
+
+// Date 객체
+var now = new Date(); 
+now.toDateString();
+
+// Math 객체
+Math.random(); 
+
+// window 객체
+window.open("경로", "창 이름", "창 옵션");
+
+// navigator 객체
+// battery, cookieEnabled, geolocation ...
+
+// history 객체 (읽기 전용)
+// back() forward() go(-1)
+
+// location 객체
+
+// screen 객체
